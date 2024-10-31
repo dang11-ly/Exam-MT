f745d5439dc36803270d0b3aa60922c5694dd35b
dang11-ly <layaoamandayvelle@gmail.com>
2024-10-31 07:52:08 -0400

    Update FavNumberCalculation.js

--- FavNumberCalculation.js
+++ FavNumberCalculation.js
@@ -1,5 +1,4 @@
-// Assuming the user's favorite number is stored in a variable called 'theFavNumber'
-let theFavNumber = 10; // Replace with the actual favorite number
+let theFavNumber = 10; 
 
 let guess = prompt("Guess my favorite number:");
 while (parseInt(guess) !== theFavNumber) {
