6c44dd8244feddc652ade649f964169c04a0c0f0
dang11-ly <layaoamandayvelle@gmail.com>
2024-10-31 07:44:18 -0400

    Add files via upload

---
+++ userProfile.js
@@ -0,0 +1,11 @@
+// Collect the user information
+var fullName = "Amanda Yvelle Layao";
+var age = 19;
+var favoriteNumber = 30;
+var favoriteColor = "Black";
+
+// Log the information to the console
+console.log(fullName);
+console.log(age);
+console.log(favoriteNumber);
+console.log(favoriteColor);
