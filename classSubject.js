dd9cd158542486dd99efd137a06f8b9c9fc0ff25
dang11-ly <layaoamandayvelle@gmail.com>
2024-10-31 07:47:50 -0400

    Add files via upload

---
+++ classSubject.js
@@ -0,0 +1,7 @@
+var subjectTitle = "Data Structures and Algorithms";
+var classSchedule = "10:00 A.m, M & F. 8:00 - 9:30 A.m, T & Th";
+var classroom = "Computer lab. B";
+var classInstructor = "C.O Pascua";
+var studentName = "Amanda Yvelle Layao";
+
+console.log(`${studentName} is currently enrolled in ${subjectTitle} with a class schedule of ${classSchedule} at room ${classroom}, The instructor for the subject is ${classInstructor}`);
