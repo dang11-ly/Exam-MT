0d009f47bb3bb1ae4c9fe9a76094a247f94b73a1
dang11-ly <layaoamandayvelle@gmail.com>
2024-10-31 11:06:52 -0400

    Add files via upload

---
+++ ColorPreferences.js
@@ -0,0 +1,6 @@
+var favoriteColors = ["Black","Armygreen","White"];
+
+var newColor = prompt("Enter your favorite color:");
+favoriteColors.push(newColor);
+
+console.log("Favorite Colors:",favoriteColors);
